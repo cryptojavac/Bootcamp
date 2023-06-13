953cc77cb32373bc2521a8e86877ec822f6eedfd
Hola <Cryptojavac@gmail.com>
2023-06-13 07:19:11 -0500

    Cree un Hola Mundo

--- Clase_5/Main.java
+++ Clase_5/Main.java
@@ -1,8 +1,14 @@
 package Clase_5;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-    System.out.println("hola mundo");
+        Scanner cargaDeDatos = new Scanner(System.in);
 
+        System.out.println("Cuantos años de vida tiene el perro: ");
+        double edadDelPerro = cargaDeDatos.nextDouble();
+        double real = 7 * edadDelPerro;
+        System.out.println("La Edad de Perro es " + real);
     }
 }
